ace56e874f6e824d5554c09de9edba400aa65c97
Luek9889 <lpearson345610@gmail.com>
2022-02-22 11:09:30 +0000

    added year to add record

--- add_record.php
+++ add_record.php
@@ -4,10 +4,11 @@
 $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
 $name = filter_input(INPUT_POST, 'name');
 $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
+$year = filter_input(INPUT_POST, 'year', FILTER_VALIDATE_INT);
 
 // Validate inputs
 if ($category_id == null || $category_id == false ||
-    $name == null || $price == null || $price == false ) {
+    $name == null || $price == null || $price == false || $year == null || $year == false ) {
     $error = "Invalid product data. Check all fields and try again.";
     include('error.php');
     exit();
@@ -63,12 +64,13 @@ if ($category_id == null || $category_id == false ||
 
     // Add the product to the database 
     $query = "INSERT INTO records
-                 (categoryID, name, price, image)
+                 (categoryID, name, price, image, year)
               VALUES
-                 (:category_id, :name, :price, :image)";
+                 (:category_id, :name, :price, :image, :year)";
     $statement = $db->prepare($query);
     $statement->bindValue(':category_id', $category_id);
     $statement->bindValue(':name', $name);
+    $statement->bindValue(':year', $year);
     $statement->bindValue(':price', $price);
     $statement->bindValue(':image', $image);
     $statement->execute();
