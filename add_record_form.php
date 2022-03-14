6674f427cd4541985b1991114d0ef6001906e75c
Luek9889 <lpearson345610@gmail.com>
2022-03-14 00:50:01 +0000

    Fixed the nav bar in the add record form to reflect the other nav bar throughout the website

--- add_record_form.php
+++ add_record_form.php
@@ -23,12 +23,13 @@ $statement->closeCursor();
 <header><h1>Collectibles and Their Prices</h1></header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Categories</a>
+    <a class="navbar-brand" href="index.php">Home</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="category_list.php">Categories</a></li>
         <?php foreach ($categories as $category) : ?>
         <li class="nav-item"><a class="nav-link" href=".?category_id=<?php echo $category['categoryID']; ?>">
         <?php echo $category['categoryName']; ?>
